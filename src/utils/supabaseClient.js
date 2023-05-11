38660a92e963367aa4135a829dab7a964d23c897
phandinhthanhhoan <thanhhoan120702@gmail.com>
2023-05-11 22:36:40 +0700

    🔧 chore(supabaseClient.js): replace environment variables with hardcoded values for supabaseUrl and supabaseAnonKey

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://qbzwydkwervovqojspyx.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFiend5ZGt3ZXJ2b3Zxb2pzcHl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM1MjI5ODYsImV4cCI6MTk5OTA5ODk4Nn0.f6Raj9UT4KUP2autC1e_G3ZUutP17mEBoXOO7KULF78";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
